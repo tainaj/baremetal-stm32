a1b8ccd496a3312bd817ddab0ab5f18bc9d96b7e
Joel Taina <jtaina124@gmail.com>
2022-01-17 22:43:36 -0500

    Lab 3 - Uploaded submit box

---
+++ part3/src/main.h
@@ -0,0 +1,13 @@
+#ifndef _VVC_MAIN_H
+#define _VVC_MAIN_H
+
+#include <stdint.h>
+#include "stm32f051x8.h"
+
+// Define GPIOB pin mappings for our button.
+#define BUTTON_PIN (12)
+
+// Define GPIOC pin mappings for our LED.
+#define LED_PIN    (9)
+
+#endif
