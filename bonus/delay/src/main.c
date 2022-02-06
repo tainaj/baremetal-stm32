fefa9df396a99a521c879c686aa10c441e93271d
Joel Taina <jtaina124@gmail.com>
2022-02-06 16:22:01 -0500

    delay - correct to 48MHz for F0

--- bonus/delay/src/main.c
+++ bonus/delay/src/main.c
@@ -4,6 +4,60 @@
  * Main program.
  */
 int main(void) {
+  // Initial clock setup.
+  #ifdef VVC_F0
+    // Reset the Flash 'Access Control Register', and
+    // then set 1 wait-state and enable the prefetch buffer.
+    // (The device header files only show 1 bit for the F0
+    //  line, but the reference manual shows 3...)
+    FLASH->ACR &= ~(0x00000017);
+    FLASH->ACR |=  (FLASH_ACR_LATENCY |
+                    FLASH_ACR_PRFTBE);
+    // Configure the PLL to (HSI / 2) * 12 = 48MHz.
+    // Use a PLLMUL of 0xA for *12, and keep PLLSRC at 0
+    // to use (HSI / PREDIV) as the core source. HSI = 8MHz.
+    RCC->CFGR  &= ~(RCC_CFGR_PLLMUL |
+                    RCC_CFGR_PLLSRC);
+    RCC->CFGR  |=  (RCC_CFGR_PLLSRC_HSI_DIV2 |
+                    RCC_CFGR_PLLMUL12);
+    // Turn the PLL on and wait for it to be ready.
+    RCC->CR    |=  (RCC_CR_PLLON);
+    while (!(RCC->CR & RCC_CR_PLLRDY)) {};
+    // Select the PLL as the system clock source.
+    RCC->CFGR  &= ~(RCC_CFGR_SW);
+    RCC->CFGR  |=  (RCC_CFGR_SW_PLL);
+    while (!(RCC->CFGR & RCC_CFGR_SWS_PLL)) {};
+    // Set the global clock speed variable.
+    //core_clock_hz = 48000000;
+  #elif VVC_L0
+    // Set the Flash ACR to use 1 wait-state
+    // and enable the prefetch buffer and pre-read.
+    FLASH->ACR |=  (FLASH_ACR_LATENCY |
+                    FLASH_ACR_PRFTEN |
+                    FLASH_ACR_PRE_READ);
+    // Enable the HSI oscillator, since the L0 series boots
+    // to the MSI one.
+    RCC->CR    |=  (RCC_CR_HSION);
+    while (!(RCC->CR & RCC_CR_HSIRDY)) {};
+    // Configure the PLL to use HSI16 with a PLLDIV of
+    // 2 and PLLMUL of 4.
+    RCC->CFGR  &= ~(RCC_CFGR_PLLDIV |
+                    RCC_CFGR_PLLMUL |
+                    RCC_CFGR_PLLSRC);
+    RCC->CFGR  |=  (RCC_CFGR_PLLDIV2 |
+                    RCC_CFGR_PLLMUL4 |
+                    RCC_CFGR_PLLSRC_HSI);
+    // Enable the PLL and wait for it to stabilize.
+    RCC->CR    |=  (RCC_CR_PLLON);
+    while (!(RCC->CR & RCC_CR_PLLRDY)) {};
+    // Select the PLL as the system clock source.
+    RCC->CFGR  &= ~(RCC_CFGR_SW);
+    RCC->CFGR  |=  (RCC_CFGR_SW_PLL);
+    while (!(RCC->CFGR & RCC_CFGR_SWS_PLL)) {};
+    // Set the global clock speed variable.
+    //core_clock_hz = 32000000;
+  #endif
+
   #ifdef VVC_F0
     // Enable the GPIOC peripheral in 'RCC_AHBENR'.
     RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
